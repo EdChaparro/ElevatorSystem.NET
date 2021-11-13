5708458fe1353207c5d06c91090b29f02d2e021e
Ed Chaparro <ed@IntrepidProducts.com>
2021-11-13 16:32:17 -0500

    Add initial Elevator class and enums

---
+++ src/ElevatorSystem/Elevators/DoorStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace IntrepidProducts.ElevatorSystem.Elevators
+{
+    public enum DoorStatus
+    {
+        //TODO: Consider IsOpening and IsClosing States
+        Open = 0,
+        Closed = 1
+    }
+}
