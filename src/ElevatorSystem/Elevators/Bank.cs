223a3e379616dc82eaa696cf091b482d7b3dd48d
Ed Chaparro <ed@IntrepidProducts.com>
2022-01-08 23:47:01 -0500

    Simplify Low & High Elevator Bank Floor Number

--- src/ElevatorSystem/Elevators/Bank.cs
+++ src/ElevatorSystem/Elevators/Bank.cs
@@ -80,8 +80,8 @@ namespace IntrepidProducts.ElevatorSystem.Elevators
         public int NumberOfFloors => _floors.Count;
         public IEnumerable<int> OrderedFloorNumbers => _floors.Keys.OrderBy(x => x);
 
-        public int LowestFloorNbr => _floors.Any() ? _floors.Keys.Min() : 0;
-        public int HighestFloorNbr => _floors.Any() ? _floors.Keys.Max() : 0;
+        public int LowestFloorNbr => OrderedFloorNumbers.Min();
+        public int HighestFloorNbr => OrderedFloorNumbers.Max();
 
         public override string ToString()
         {
