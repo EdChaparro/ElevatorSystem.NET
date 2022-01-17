06cff6ed79c9c21e4c2e109cf1ef81dcf4f4f61a
Ed Chaparro <ed@IntrepidProducts.com>
2022-01-17 12:24:26 -0500

    Introduce Elevator Floor Request Button Panel

---
+++ src/ElevatorSystem/Buttons/ElevatorFloorRequestButton.cs
@@ -0,0 +1,12 @@
+﻿namespace IntrepidProducts.ElevatorSystem.Buttons
+{
+    public class ElevatorFloorRequestButton : AbstractButton
+    {
+        public ElevatorFloorRequestButton(int floorNbr)
+        {
+            FloorNbr = floorNbr;
+        }
+
+        public int FloorNbr { get; }
+    }
+}
---
+++ src/Tests/Test.ElevatorSystem/Buttons/ElevatorFloorRequestPanelTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using IntrepidProducts.ElevatorSystem.Buttons;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace IntrepidProducts.ElevatorSystem.Tests.Buttons
+{
+    [TestClass]
+    public class ElevatorFloorRequestPanelTest
+    {
+        [TestMethod]
+        public void ShouldHaveButtonForEachFloor()
+        {
+            var floorRequestPanel = new ElevatorFloorRequestPanel(1, 2, 3);
+
+            Assert.AreEqual(3, floorRequestPanel.NumberOfButtons);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ShouldRequreAtLeastTwoFloors()
+        {
+            var floorRequestPanel = new ElevatorFloorRequestPanel();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ShouldRejectFloorNumbersLessThanOne()
+        {
+            var floorRequestPanel = new ElevatorFloorRequestPanel(0, 1, 2);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ShouldRequireUniqueFloorNumbers()
+        {
+            var floorRequestPanel = new ElevatorFloorRequestPanel(1, 2, 2, 3);
+        }
+    }
+}
