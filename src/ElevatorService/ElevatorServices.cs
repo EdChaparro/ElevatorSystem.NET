d5ade04637b5f30e7150bbc847d77380a6f03b6b
Ed Chaparro <ed@IntrepidProducts.com>
2023-03-02 18:55:58 -0500

    Move class inside namespace braces

--- src/ElevatorService/ElevatorServices.cs
+++ src/ElevatorService/ElevatorServices.cs
@@ -1,105 +1,106 @@
 using IntrepidProducts.ElevatorSystem.Elevators;
 
-namespace IntrepidProducts.ElevatorService;
-
-public interface IElevatorServices
+namespace IntrepidProducts.ElevatorService
 {
-    int Count { get; }
-
-    void Register(params Elevator[] elevators);
-    void UnRegister(params Elevator[] elevators);
-    bool IsRegistered(Elevator elevator);
-
-    bool Start(Elevator elevator);
-    Task<bool> StopAsync(Elevator elevator);
-
-    bool IsRunning(Elevator elevator);
-}
-
-public class ElevatorServices : IElevatorServices
-{
-    private readonly Dictionary<Guid, (ElevatorService service, CancellationToken cancellationToken)>
-        _serviceDetails = new();
-
-    public int Count => _serviceDetails.Count;
-
-    public void Register(params Elevator[] elevators)
+    public interface IElevatorServices
     {
-        foreach (var elevator in elevators)
+        int Count { get; }
+
+        void Register(params Elevator[] elevators);
+        void UnRegister(params Elevator[] elevators);
+        bool IsRegistered(Elevator elevator);
+
+        bool Start(Elevator elevator);
+        Task<bool> StopAsync(Elevator elevator);
+
+        bool IsRunning(Elevator elevator);
+    }
+
+    public class ElevatorServices : IElevatorServices
+    {
+        private readonly Dictionary<Guid, (ElevatorService service, CancellationToken cancellationToken)>
+            _serviceDetails = new();
+
+        public int Count => _serviceDetails.Count;
+
+        public void Register(params Elevator[] elevators)
         {
-            if (_serviceDetails.ContainsKey(elevator.Id))
+            foreach (var elevator in elevators)
             {
-                continue;
+                if (_serviceDetails.ContainsKey(elevator.Id))
+                {
+                    continue;
+                }
+
+                //TODO: Use IoC
+                _serviceDetails.Add(elevator.Id, (new ElevatorService(elevator), new CancellationToken()));
             }
-
-            //TODO: Use IoC
-            _serviceDetails.Add(elevator.Id, (new ElevatorService(elevator), new CancellationToken()));
         }
-    }
 
-    public IBackgroundService? Get(Elevator elevator)
-    {
-        return _serviceDetails.ContainsKey(elevator.Id) ? _serviceDetails[elevator.Id].service : null;
-    }
+        public IBackgroundService? Get(Elevator elevator)
+        {
+            return _serviceDetails.ContainsKey(elevator.Id) ? _serviceDetails[elevator.Id].service : null;
+        }
 
-    public void UnRegister(params Elevator[] elevators)
-    {
-        foreach (var elevator in elevators)
+        public void UnRegister(params Elevator[] elevators)
+        {
+            foreach (var elevator in elevators)
+            {
+                if (!_serviceDetails.ContainsKey(elevator.Id))
+                {
+                    continue;
+                }
+
+                var (service, cancellationToken) = _serviceDetails[elevator.Id];
+
+                if (service.IsRunning)
+                {
+                    service.StopAsync(cancellationToken);
+                }
+
+                _serviceDetails.Remove(elevator.Id);
+            }
+        }
+
+        public bool IsRegistered(Elevator elevator)
+        {
+            return _serviceDetails.ContainsKey(elevator.Id);
+        }
+
+        public bool Start(Elevator elevator)
         {
             if (!_serviceDetails.ContainsKey(elevator.Id))
             {
-                continue;
+                return false;
+            }
+
+            var (service, cancellationToken) = _serviceDetails[elevator.Id];
+            service.StartAsync(cancellationToken);
+            return true;
+        }
+
+        public async Task<bool> StopAsync(Elevator elevator)
+        {
+            if (!_serviceDetails.ContainsKey(elevator.Id))
+            {
+                return false;
             }
 
             var (service, cancellationToken) = _serviceDetails[elevator.Id];
 
-            if (service.IsRunning)
+            if (!service.IsRunning)
             {
-                service.StopAsync(cancellationToken);
+                return true;
             }
 
-            _serviceDetails.Remove(elevator.Id);
-        }
-    }
-
-    public bool IsRegistered(Elevator elevator)
-    {
-        return _serviceDetails.ContainsKey(elevator.Id);
-    }
-
-    public bool Start(Elevator elevator)
-    {
-        if (!_serviceDetails.ContainsKey(elevator.Id))
-        {
-            return false;
-        }
-
-        var (service, cancellationToken) = _serviceDetails[elevator.Id];
-        service.StartAsync(cancellationToken);
-        return true;
-    }
-
-    public async Task<bool> StopAsync(Elevator elevator)
-    {
-        if (!_serviceDetails.ContainsKey(elevator.Id))
-        {
-            return false;
-        }
-
-        var (service, cancellationToken) = _serviceDetails[elevator.Id];
-
-        if (!service.IsRunning)
-        {
+            await service.StopAsync(cancellationToken);
             return true;
         }
 
-        await service.StopAsync(cancellationToken);
-        return true;
-    }
-
-    public bool IsRunning(Elevator elevator)
-    {
-        return _serviceDetails.ContainsKey(elevator.Id) &&
-               _serviceDetails[elevator.Id].service.IsRunning;
+        public bool IsRunning(Elevator elevator)
+        {
+            return _serviceDetails.ContainsKey(elevator.Id) &&
+                   _serviceDetails[elevator.Id].service.IsRunning;
+        }
     }
 }
