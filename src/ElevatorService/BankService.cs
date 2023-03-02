c8cf5c0289589499d3791d71c85c630ac67ce686
Ed Chaparro <ed@IntrepidProducts.com>
2023-03-02 18:07:47 -0500

    Implement Bank Background Service
    Not wired up yet

---
+++ src/ElevatorService/BankService.cs
@@ -0,0 +1,71 @@
+using IntrepidProducts.ElevatorSystem.Banks;
+using IntrepidProducts.ElevatorSystem.Elevators;
+
+namespace IntrepidProducts.ElevatorService;
+
+public class BankService : AbstractBackgroundService
+{
+    public BankService(Bank bank)
+    {
+        SleepIntervalInMilliseconds = Configuration.EngineSleepIntervalInMilliseconds;
+        Bank = bank;
+        Strategy = new IdleStrategy(bank, new ProximateStrategy(bank));  //TODO: use IoC
+    }
+
+    private Bank Bank { get; }
+    private IStrategy Strategy { get; }
+
+    protected int SleepIntervalInMilliseconds { get; set; }
+    public List<RequestedFloorStop> AssignedFloorStops { get; private set; } = new();
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        Console.WriteLine($"Real BankService started at: {DateTimeOffset.Now}, Id: {Bank.Id}");
+        Console.WriteLine($"SleepIntervalInMilliseconds {SleepIntervalInMilliseconds}");
+
+
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            ClearCompletedFloorStops();
+
+            AssignedFloorStops.AddRange(AssignElevators(Direction.Down));
+            AssignedFloorStops.AddRange(AssignElevators(Direction.Up));
+
+            Console.WriteLine($"Real BankService running at: {DateTimeOffset.Now}");
+
+            await Task.Delay(SleepIntervalInMilliseconds, stoppingToken);
+        }
+    }
+
+    private void ClearCompletedFloorStops()
+    {
+        var expiredAssignments =
+            AssignedFloorStops.Except(PendingElevatorStops);
+
+        AssignedFloorStops = AssignedFloorStops.Except(expiredAssignments).ToList();
+    }
+
+    private IEnumerable<RequestedFloorStop> PendingElevatorStops
+    {
+        get
+        {
+            var pendingElevatorStops = new List<RequestedFloorStop>();
+
+            foreach (var e in Bank.Elevators)
+            {
+                pendingElevatorStops.AddRange(e.RequestedFloorStops.ToList());
+            }
+
+            return pendingElevatorStops;
+        }
+    }
+
+    private List<RequestedFloorStop> AssignElevators(Direction direction)
+    {
+        var requestedFloorStops = Bank.GetRequestedFloorStops(direction)
+            .Select(x => x.FloorNbr).ToList();
+
+        return Strategy.AssignElevators(requestedFloorStops, direction).ToList();
+    }
+
+}
---
+++ src/ElevatorService/BankServiceConsoleRunner.cs
@@ -0,0 +1,31 @@
+using IntrepidProducts.ElevatorSystem.Banks;
+
+namespace IntrepidProducts.ElevatorService;
+
+public class BankServiceConsoleRunner
+{
+    public static void Main(string[] args)
+    {
+        var bank = new Bank(2, 1..10);
+
+        var service = GetEngineFor(bank);
+
+        var cancellationToken = new CancellationToken();
+        service.StartAsync(cancellationToken);
+
+        Console.WriteLine("Bank Engine started directly, waiting 5 second before shutdown");
+        Console.WriteLine($"IsRunning: {service.IsRunning}");
+
+        Thread.Sleep(5000);
+
+        Console.WriteLine("Shutting down Engine");
+        service.StopAsync(cancellationToken);
+
+        Console.WriteLine($"IsRunning: {service.IsRunning}");
+    }
+
+    public static IBackgroundService GetEngineFor(Bank bank)
+    {
+        return new BankService(bank);
+    }
+}
